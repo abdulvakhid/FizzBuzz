ddee052095a536b657bff93edcbe57f950523a63
abdulvakhid <buriyevabdulvohid055@gmail.com>
2022-09-26 17:11:14 +0500

    Commit

--- main.js
+++ main.js
@@ -8,6 +8,7 @@ var elOutputFB = document.querySelector(".outputfb");
 var fizzlength = document.querySelector(".fizzlength");
 var buzzlength = document.querySelector(".buzzlength");
 var fizzbuzzlength = document.querySelector(".fizzbuzzlength");
+var elSpan = document.querySelector(".element");
 // console.log(fizzbuzzlength);
 
 var fizbuz = [];
@@ -16,31 +17,32 @@ var buz = [];
 
 
 elFizzBuzz.addEventListener("submit", function (evt) {
-
+    
     evt.preventDefault();
-
-    var inputValue = elInput.value;
-
-
-    if (inputValue % 3 == 0 && inputValue % 5 == 0) {
+    
+    var inputValue = Number( elInput.value);
+    
+    if (isNaN(inputValue) || inputValue === 0){
+    alert("Son kiritmadingiz !!!")
+    }else if (inputValue % 3 == 0 && inputValue % 5 == 0) {
         fizbuz.push(inputValue);
         elOutputFB.textContent = fizbuz;
         fizzbuzzlength.textContent = fizbuz.length;
-
+        
     } else if (inputValue % 3 == 0) {
         fiz.push(inputValue);
         elOutputF.textContent = fiz;
         fizzlength.textContent = fiz.length;
-
-
+        
+        
     } else if (inputValue % 5 == 0) {
         buz.push(inputValue);
         elOutputB.textContent = buz;
         buzzlength.textContent = buz.length;
-
+        
     } else {
         alert("Bu son 3 va 5 ga bo'linmaydi !")
     }
-
+    
     elInput.value = "";
 })
